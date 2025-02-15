fb50c2d6ed609894a8f4b83d56745f46b1be3efc
LT-Schmiddy <schmidalex1996@gmail.com>
2025-02-15 16:07:30 -0500

    generalizing for non-mm recomps

--- build_mod.py
+++ build_mod.py
@@ -6,14 +6,14 @@ USING_ASSETS_ARCHIVE = True
 
 project_root = pathlib.Path(__file__).parent
 
-mod_data = tomllib.loads(project_root.joinpath("mod.toml").read_text())
-mod_manifest_data = mod_data["manifest"]
-# print(mod_data)
-build_dir = project_root.joinpath(f"build")
-build_nrm_file = build_dir.joinpath(f"{mod_data['inputs']['mod_filename']}.nrm")
+# mod_data = tomllib.loads(project_root.joinpath("mod.toml").read_text())
+# mod_manifest_data = mod_data["manifest"]
+# # print(mod_data)
+# build_dir = project_root.joinpath(f"build")
+# build_nrm_file = build_dir.joinpath(f"{mod_data['inputs']['mod_filename']}.nrm")
 
-runtime_mods_dir = project_root.joinpath("runtime/mods")
-runtime_nrm_file = runtime_mods_dir.joinpath(f"{mod_data['inputs']['mod_filename']}.nrm")
+# runtime_mods_dir = project_root.joinpath("runtime/mods")
+# runtime_nrm_file = runtime_mods_dir.joinpath(f"{mod_data['inputs']['mod_filename']}.nrm")
 
 assets_archive_path = project_root.joinpath("assets_archive.zip")
 assets_extract_path = project_root.joinpath("assets_extracted/assets")
@@ -30,27 +30,11 @@ def build_elf(makefile_path: Path):
     if make_run.returncode != 0:
         raise RuntimeError("Make failed to build mod binaries.")
 
-
-def run_build():
-    # Unzipping Archive:
-    if USING_ASSETS_ARCHIVE and not assets_extract_path.is_dir():
-        print(f"Assets folder '{assets_extract_path.name}' not found. Extracting assets from '{assets_archive_path.name}'...")
-        with zipfile.ZipFile(assets_archive_path, 'r') as zip_ref:
-            zip_ref.extractall(assets_extract_path)
-            
-    
-    if not bnt.build_dir.exists():
-        print("N64Recomp tools not built. Building now...")
-        bnt.rebuild_tools();
-
-    deps = bnt.deps
-
-
-
+def build_mod_toml(toml_path: Path):
     RecompModTool_run = subprocess.run(
         [
             bnt.get_RecompModTool_path(),
-            "mod.toml",
+            str(toml_path),
             "build"
         ],
         cwd=pathlib.Path(__file__).parent
@@ -58,11 +42,18 @@ def run_build():
     if RecompModTool_run.returncode != 0:
         raise RuntimeError("RecompModTool failed to build mod.")
 
+    
+def run_build():          
+    if not bnt.build_dir.exists():
+        print("N64Recomp tools not built. Building now...")
+        bnt.rebuild_tools();
 
+    build_elf(Path("./Makefile_ctype"))
+    build_mod_toml(Path("./tomls/mm/ctype.toml"))
 
     # Copying files for debugging:
-    os.makedirs(runtime_mods_dir, exist_ok=True)
-    shutil.copy(build_nrm_file, runtime_nrm_file)
+    # os.makedirs(runtime_mods_dir, exist_ok=True)
+    # shutil.copy(build_nrm_file, runtime_nrm_file)
 
 if __name__ == '__main__':
     run_build()
